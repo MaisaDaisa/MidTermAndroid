dc3fd73cce3ea784c19720655488afce0f6d9c71
Nick <maisadzenika@gmail.com>
2026-04-18 22:56:45 +0400

    ref: improve design of fragment b

--- app/src/main/java/com/example/midtermandroid/FragmentB.kt
+++ app/src/main/java/com/example/midtermandroid/FragmentB.kt
@@ -2,27 +2,25 @@ package com.example.midtermandroid
 
 import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.midtermandroid.databinding.FragmentBBinding
 
 class FragmentB : Fragment() {
 
-    lateinit var binding: FragmentBBinding
+    private lateinit var binding: FragmentBBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        binding = FragmentBBinding.inflate(layoutInflater)
+        binding = FragmentBBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentBBinding.bind(view)
 
         val salary = arguments?.getDouble("NM_SALARY") ?: 0.0
         val rent = arguments?.getDouble("NM_RENT") ?: 0.0
@@ -32,17 +30,16 @@ class FragmentB : Fragment() {
         val result = manager.calculate(salary, rent, food)
 
         binding.nmTxtExpensesResult.text = getString(R.string.nm_expenses_format, result.expenses)
-        binding.nmTxtSavingsResult.text = getString(R.string.nm_savings_format, result.savings)
+
 
         if (result.isPositive) {
+            binding.nmTxtSavingsResult.text = getString(R.string.nm_savings_format, result.savings)
             binding.nmTxtExpensesResult.setTextColor(Color.GREEN)
             binding.nmTxtSavingsResult.setTextColor(Color.GREEN)
         } else {
+            binding.nmTxtSavingsResult.text = getString(R.string.nm_loss_format, result.savings)
             binding.nmTxtExpensesResult.setTextColor(Color.RED)
             binding.nmTxtSavingsResult.setTextColor(Color.RED)
         }
-
     }
-
-
 }
